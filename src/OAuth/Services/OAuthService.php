ce745b87390652c0135084f27a0d49edffd83643
Jacopo <82162704+dinacea93@users.noreply.github.com>
2024-05-20 12:17:53 +0200

    Update OAuthService.php

--- src/OAuth/Services/OAuthService.php
+++ src/OAuth/Services/OAuthService.php
@@ -116,6 +116,10 @@ class OAuthService
             'sandbox' => [
                 'valid'   => ['bool'],
                 'default' => false
+            ],
+            'scope' => [
+                'valid'    => ['string'],
+                'required' => false
             ]
         ];
     }
@@ -271,6 +275,11 @@ class OAuthService
         }
         if (!isset($request->scope)) {
             $request->scope = 'https://api.ebay.com/oauth/api_scope';
+            if($this->getConfig('scope') <> NULL){
+                $request->scope = $this->getConfig('scope');
+            }else{
+                $request->scope = 'https://api.ebay.com/oauth/api_scope';
+            }
         }
 
         return $this->callOperationAsync('getAppToken', $request);
